017be364885cea4115c828050db0554a243b5434
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2025-04-27 15:50:54 -0500

    Added flexbox to images, created containers, put images into the repo itself. Will eventually create and array of objects for blog posts API style to serve and make room for more blog posts

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 alert("Welcome!")
-$("button").on("click",
-  function()
-    {alert("Thank you for reading!")});
-$("h2").on("click",
-  function()
-    {alert("You can read more on this at kujo-sakana.jp")});
-$("img").on("click",
-  function()
-    {alert("Source for copyrighted images can be found below!")});
-$("p").on("click",
-  function()
-    {alert("References can be found at the bottom of the page!")});
 
-console.log("It's alive!")
+const blogButton = document.querySelector('button')
+const blogTitle = document.querySelector('h2')
+const blogImage = document.querySelector('img')
+const blogParagraph = document.querySelector('p')
+
+blogButton.addEventListener('click', async () => {
+      alert("Thank you for reading!")
+  }
+)
+
+blogTitle.addEventListener('click', async () => {
+      alert("You can read more on this at kujo-sakana.jp")
+  }
+)
+blogImage.addEventListener('click', async () => {
+      alert("Source for copyrighted images can be found below!")
+  }
+)
+blogParagraph.addEventListener('click', async () => {
+      alert("References can be found at the bottom of the page!")
+  }
+)
+
+// Depricated jQuery code
+
+// $("button").on("click",
+//   function()
+//     {alert("Thank you for reading!")})
+// $("h2").on("click",
+//   function()
+//     {alert("You can read more on this at kujo-sakana.jp")})
+// $("img").on("click",
+//   function()
+//     {alert("Source for copyrighted images can be found below!")})
+// $("p").on("click",
+//   function()
+//     {alert("References can be found at the bottom of the page!")})
